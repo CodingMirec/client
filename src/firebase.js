ee05af1bf8effa7287de5636ad2fe2ae74a168e2
CodingMirec <m.sykora01@gmail.com>
2021-12-31 13:31:35 +0100

    init firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpRtz2s9K_b6ZtecUjGrnBQlK3l4ZHFJ8",
+  authDomain: "messageappmern.firebaseapp.com",
+  projectId: "messageappmern",
+  storageBucket: "messageappmern.appspot.com",
+  messagingSenderId: "479215575764",
+  appId: "1:479215575764:web:a47f1ebecc841b91191141",
+  measurementId: "G-XC7G5HS6E1",
+};
