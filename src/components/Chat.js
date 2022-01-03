537538450083783fc7ffa5502db1e18b9c052a9a
CodingMirec <m.sykora01@gmail.com>
2022-01-03 22:26:12 +0100

    chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { Avatar, IconButton } from "@mui/material";
 import "./Chat.css";
+
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import AttachFileOutlinedIcon from "@mui/icons-material/AttachFileOutlined";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>This is chat</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFileOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertOutlinedIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Johny</span>
+          this is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
     </div>
   );
 }
