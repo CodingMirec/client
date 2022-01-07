f666bd966399ef467c078b6a05c27834c42c208e
CodingMirec <m.sykora01@gmail.com>
2022-01-07 19:30:32 +0100

    chat done

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,6 +5,8 @@ import "./Chat.css";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import AttachFileOutlinedIcon from "@mui/icons-material/AttachFileOutlined";
 import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import InsertEmoticonOutlinedIcon from "@mui/icons-material/InsertEmoticonOutlined";
+import MicOutlinedIcon from "@mui/icons-material/MicOutlined";
 
 function Chat() {
   return (
@@ -36,6 +38,24 @@ function Chat() {
           this is message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Johny</span>
+          this is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+        <p className="chat__message">
+          <span className="chat__name">Johny</span>
+          this is message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonOutlinedIcon />
+        <form action="">
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicOutlinedIcon />
       </div>
     </div>
   );
